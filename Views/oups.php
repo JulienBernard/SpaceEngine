770c87153d53d06002f396442f840a52db25bab3
JulienBernard <pro.julienbernard@free.fr>
2013-10-12 23:01:00 +0200

    Ajout de la page "oups" qui se manifeste si l'utilisateur essaye d'accèder à une page où il n'a pas accès.

---
+++ Views/oups.php
@@ -0,0 +1,22 @@
+
+	<main id="main">
+		<section class="primary" id="oups">
+			<article class="article">
+				<span class="fast-link">
+					<a class="toTop"><img src="./img/up.png" alt="Previous" /></a>
+					<a class="toFollow"><img src="./img/down.png" alt="Next" /></a>
+				</span>
+				<h1><img src="./img/home.png" /> Oups!</h1>
+				<p>
+					You haven't the necessary rank to see this page!
+				</p>
+				<p>
+					Please, <a href="index.php">return to the homepage</a>.
+				</p>
+			</article>
+		</section>
+	</main>
+	
+	<script type="text/javascript">redirection(3, 'index.php');</script>
+	
+	
---
+++ oups.php
@@ -0,0 +1,30 @@
+<?php
+
+	/***
+	 * 
+	 * Point d'entrée de la page d'accueil.
+	 * @author JulienBernard
+	 * 
+	 */
+	
+	/* Le namePage permet d'identifier votre page. Il doit être être écrit en minuscule et tenir en un seul mot.
+	 */
+	$namePage = "oups";
+	
+	/* Appel du moteur [ne pas modifier] */
+	include_once("./config.php");
+	
+	$Engine = new Engine( $namePage );
+	$Template = new Template();
+	
+	/* Informations sur la page [valeurs à modifier] */
+	$Template->setTitle("Oups!");
+	$Template->setDescription("Site propulsé par le SpaceEngine ! SpaceEngine Copyright (C) 2013 Julien Bernard - SpaceEngine is a free website engine under GPL license!");
+	$Template->addCss("normalize.css");
+	$Template->addCss("foundation.min.css");
+	$Template->addSCript("jquery.min.js");
+	$Template->addSCript("redirection.js");
+	
+	/* Lancement du moteur [ne pas modifier] */
+	$Engine->startEngine( $Engine, $Template );
+?>
